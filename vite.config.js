7e2d120268ece580de40bf9d8344402521f8a33d
mrgconstruction100-max <mrgindia2020@gmail.com>
2025-11-04 15:36:33 +0530

    Added manifest and icons for app

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  // server: {
-  //   host: "::",
-  //   port: 5173,
-  // },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Construction OMT',
+        short_name: 'COMT',
+        description: 'Construction Project Management Tool',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1E3A8A',
+        icons: [
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
